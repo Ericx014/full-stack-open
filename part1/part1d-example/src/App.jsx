381914ab5903916fcd0be3bdc12753b0731dc113
Eric Bong <ericbong6868@gmail.com>
2023-11-27 16:59:00 +0800

    updated

--- part1/part1d-example/src/App.jsx
+++ part1/part1d-example/src/App.jsx
@@ -1,38 +1,66 @@
 import { useState } from "react"
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  // const [clicks, setClicks] = useState({
+  //   left: 0, right: 0
+  // })
 
-  // Original
-  // const handleLeftClick = () => {
-  //   const newClicks = {
-  //     left: clicks.left + 1,
-  //     right: clicks.right
-  //   }
-  //   setClicks(newClicks)
-  // }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  // With object spread syntax
   const handleLeftClick = () => {
-    setClicks({...clicks, left: clicks.left + 1})
-    console.log(clicks)
+    setAll(allClicks.concat('L'))
+    
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
-    setClicks({...clicks, right: clicks.right + 1})
-    console.log(clicks)
+    setAll(allClicks.concat('R'))
+    
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
   }
 
   return (
     <div>
-      <button onClick={handleLeftClick}>Left</button>
-      {clicks.left}
-      {clicks.right}
-      <button onClick={handleRightClick}>Right</button>
+      {left}
+      <Button handleClick={handleLeftClick} text='Left'/>
+      <Button handleClick={handleRightClick} text='Right'/>
+      {right}
+
+      <p>Total left: {left}, Total right: {right}</p>
+      <p>Total click: {total}</p>
+      <History allClicks={allClicks}/>
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      Button press history: {props.allClicks.join(', ')}
     </div>
   )
 }
 
 export default App
+
+//  stopped at: Old React
