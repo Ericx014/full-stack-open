366f81cb483c5def0088282ef0cff90d050c834b
Eric Bong <ericbong6868@gmail.com>
2023-11-30 20:49:17 +0800

    completed 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+
   const [newName, setNewName] = useState('Eric Bong')
   const [newNumber, setNewNumber] = useState('011-33472177')
   const [filter, setNewFilter] = useState('')
+
+  const hook = () => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promised fullfiled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const addNewContact = (event) => {
     event.preventDefault()
