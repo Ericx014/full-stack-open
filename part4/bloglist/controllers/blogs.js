4e8f752e9d044a668a9c401fee113d439a12566d
Eric Bong <ericbong6868@gmail.com>
2023-12-29 15:01:43 +0800

    completed 4.15-4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1, id: 1})
 	response.json(blogs)
 })
 
@@ -16,13 +17,32 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+
+	const blog = new Blog({
+		title: body.title,
+		title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+		user:user._id
+	})
 
   if (!blog.title || !blog.url) {
     return response.status(400).end()
   }
-	const savedBlog = await blog.save()
-	response.status(201).json(savedBlog)
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
